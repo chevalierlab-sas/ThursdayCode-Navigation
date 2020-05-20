cc6a8c8b8d04e17ccb1bc0d3429070ab33b2dd41
aderesie <aderesie.m.s@gmail.com>
2020-05-21 00:33:42 +0700

    Set Detail Fragment

--- app/src/main/java/lab/chevalier/navigationexample/DetailFragment.kt
+++ app/src/main/java/lab/chevalier/navigationexample/DetailFragment.kt
@@ -5,18 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.navArgs
+import lab.chevalier.navigationexample.databinding.FragmentDetailBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class DetailFragment : Fragment() {
 
+    private val args : DetailFragmentArgs by navArgs()
+    private lateinit var binding: FragmentDetailBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_detail, container, false)
+
+        binding.tvNama.text = "Selamat datang ${args.nama}"
+
+        return binding.root
     }
 
 }
